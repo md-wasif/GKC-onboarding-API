795ad348218e8def76e7148e6128c0b0fb460677
Md_Wasif <md.wasif7894@gmail.com>
2021-10-09 18:10:27 +0530

    getAllBrands

--- controllers/brandsManagement.js
+++ controllers/brandsManagement.js
@@ -9,7 +9,6 @@ const authVerification = require('../routes/verifyToken');
 
 
 
-
 const parseJwt = async (token) => {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
@@ -21,38 +20,40 @@ const parseJwt = async (token) => {
     return JSON.parse(jsonPayload);
 };
 
+
 //UserId: "615d97f5204f20834f16f6e1"
 //BrandId: "615ee70c82b3b22d6c191fba"
 //GET /getAllBrands get all brands for user...
 router.get('/getAllBrands', async (req, res) => {
 
     const token = req.header('auth-token');
-    console.log(token);
-    const userId = await parseJwt(token);
-    console.log(userId.id);
-    let userBrands;
+    const filterId = await parseJwt(token);
+    //const userId = ObjectId(filterId.id);
+    const userId = mongoose.Types.ObjectId(filterId.id)
+    var userBrands;
     try {
+
         userBrands = await UserBrand.aggregate([{
-            $match: { user: userId.id }
+            $match: { profile: userId }
         },
         {
             $lookup: {
-                From: "brand",
+                from: "brands",
                 localField: "brand",
                 foreignField: "_id",
-                as: "brands"
+                as: "brand"
             }
         }, {
-            $unwind: "brands"
-        }
-        ]);
-
-        const brands = await brands.find({});
-        res.json(brands);
+            $unwind: "$brand"
+        }]);
+        //const userBrands = await UserBrand.find({});
+        res.json(userBrands);
     } catch (error) {
         res.json({ message: error });
     }
 })
+
+
 
 //GET /getCuisines All the Cuisines..
 router.get('/getCuisines', async (req, res) => {
@@ -79,17 +80,13 @@ router.get('/getBrands', async (req, res) => {
 });
 
 
-//var ObjectId = require('mongodb').ObjectId; 
-// var id = req.params.gonderi_id;       
-// var o_id = new ObjectId(id);
-// db.test.find({_id:o_id})
 
 //GET /getProducts get all products for brand..
 router.get('/getProducts', async (req, res) => {
 
     var brand = req.query.brand;
     try {
-        const products = await Product.find({ brand: brand}).select("-isDeleted -brand");
+        const products = await Product.find({ brand: brand }).select("-isDeleted -brand");
         res.json(products);
     } catch (error) {
         res.json({ message: error });
@@ -101,18 +98,15 @@ router.get('/getProducts', async (req, res) => {
 //POST /createBrand get brand from insert Id and return brand object with count of products..
 router.post('/createBrand', async (req, res) => {
 
-      const token = req.header('auth-token');
-      const filterId = await parseJwt(token);
-      const userId = filterId.id;
-       
+    const token = req.header('auth-token');
+    const filterId = await parseJwt(token);
+    const userId = filterId.id;
 
     const users = await UserBrand.create({
         profile: userId,
         brand: req.body.brand,
         products: req.body.products,
     });
-    console.log(users);
-    //console.log(UserBrand);
     try {
         const userbrands = await UserBrand.save();
         console.log(userbrands);
