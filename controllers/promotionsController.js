d6e98da29b0a3dca2aedfe7cf820ec318b35a600
Md_Wasif <md.wasif7894@gmail.com>
2021-10-29 14:32:32 +0530

    first commit

--- controllers/promotionsController.js
+++ controllers/promotionsController.js
@@ -37,74 +37,83 @@ router.post('/createPromotion', async (req, res) => {
 });
 
 
-router.post('/createUserPromotion', async (req, res) => {
 
-    const token = req.header('auth-token');
-    const filterId = await parseJwt(token);
-    const userId = mongoose.Types.ObjectId(filterId.id)
-    try {
-        const userPromotion = new UserPromotion({
-            user: userId,
-            promotion: req.body.promotion_id
-        })
-        await userPromotion.save();
-    } catch (error) {
-        res.json({ "code": "ERROR", message: error.message })
-    }
-})
+// router.get('/getPromotions', async (req, res) => {
 
+//     try{
 
-router.get('/getPromotions', async (req, res) => {
-
-    try{
-
-        const getAllPromotions = await Promotion.find({});
-        res.json({"code": "OK", "data": getAllPromotions});
-    }catch(error){
-        res.json({"code": "ERROR", message: error.message});
-    }
-});
+//         const getAllPromotions = await Promotion.find({});
+//         res.json({"code": "OK", "data": getAllPromotions});
+//     }catch(error){
+//         res.json({"code": "ERROR", message: error.message});
+//     }
+// });
 
 
 router.get('/getAllPromotions', async (req, res) => {
-    const token = req.header('auth-token');
-    const filterId = await parseJwt(token);
-    const userId = mongoose.Types.ObjectId(filterId.id)
-    try {
-        const savedPromotion = await UserPromotion.aggregate([{
-            $match: { user: userId }
-        }, {
-            $lookup: {
-                from: "promotions",
-                localField: "promotion",
-                foreignField: "_id",
-                as: "promotions"
-            }
-        }, {
-            $unwind: "$promotions"
-        }, {
-            $project: {
-                promotions: 1,
-                startDate: 1,
-                endDate: 1,
-                isActive: 1,
-                isDeleted: 1,
-                user: 1
-            }
-        }
-        ]);
-        let promotionArr = []
-        savedPromotion.forEach((item) => {
-            promotionArr.push(item.promotions)
-        })
-        savedPromotion[0].promotions = promotionArr
-        savedPromotion.splice(1);
 
-        res.json({ "code": "OK", "data": savedPromotion });
-    } catch (error) {
-        res.json({ "code": "Error", message: error.message })
-    }
+        // const token = req.header('auth-token');
+        // const filterId = await parseJwt(token);
+        // const userId = mongoose.Types.ObjectId(filterId.id)
+        try{
+
+           const getPromotions =  await Promotion.aggregate([{
+                $lookup: {
+                    from: "userpromotions",
+                    localField: "_id",
+                    foreignField: "promotion",
+                    as: "userpromotions"
+                }
+            }])
+            res.json({"code": "OK", "data": getPromotions});
+        }catch(error){
+            res.json({"code": "ERROR", message: error.message});
+        }
+       
 });
+
+// router.get('/getAllPromotions', async (req, res) => {
+//     const token = req.header('auth-token');
+//     const filterId = await parseJwt(token);
+//     const userId = mongoose.Types.ObjectId(filterId.id)
+//     try {
+//         //const getAllPromotions = await Promotion.find({});
+        
+
+//         const savedPromotion = await UserPromotion.aggregate([{
+//             $match: { user: userId }
+//         }, {
+//             $lookup: {
+//                 from: "promotions",
+//                 localField: "promotion",
+//                 foreignField: "_id",
+//                 as: "promotions"
+//             }
+//         }, {
+//             $unwind: "$promotions"
+//         }, {
+//             $project: {
+//                 promotions: 1,
+//                 startDate: 1,
+//                 endDate: 1,
+//                 isActive: 1,
+//                 isDeleted: 1,
+//                 user: 1
+//             }
+//         }
+//         ]);
+//         let promotionArr = []
+//         savedPromotion.forEach((item) => {
+//             promotionArr.push(item.promotions)
+//         })
+//         savedPromotion[0].promotions = promotionArr
+//         savedPromotion.splice(1);
+
+//         res.json({ "code": "OK", "data": savedPromotion });
+//     } catch (error) {
+//         res.json({ "code": "Error", message: error.message })
+//     }
+// });
 
 
 router.get('/getPromotion', async (req, res) => {
@@ -123,23 +132,30 @@ router.get('/getPromotion', async (req, res) => {
 
 
 
-router.put('/activeUserPromotion', async (req, res) => {
+router.post('/activeUserPromotion', async (req, res) => {
 
     const token = req.header('auth-token');
     const filterId = await parseJwt(token);
     const userId = mongoose.Types.ObjectId(filterId.id)
     var promotion_id = mongoose.Types.ObjectId(req.query.Id);
-    
+    const getData = req.body.isActive;
+    const getNumber = req.body.input;
     try {
-         const getData = req.body.isActive;
-         const getNumber = req.body.input;
+        
+        const userPromotion = new UserPromotion({
+            user: userId,
+             promotion: promotion_id
+        })
+         const getuserPromotion = await userPromotion.save();
+        //  console.log(getuserPromotion);
+         const userpromotion_id = getuserPromotion._id;
         await UserPromotion.updateOne({
-            user: userId, _id: promotion_id },
+            user: userId, _id: userpromotion_id },
             //{$set: { "endDate":  7 * 24 * 60 * 60000}}
             [{ $set: { "isActive": getData, endDate: { $add: ["$endDate", getNumber * 7 * 24 * 60 * 60000] } } }],
         )
 
-        const getdeactiveUser = await UserPromotion.findById(promotion_id);
+        const getdeactiveUser = await UserPromotion.findById(userpromotion_id);
         res.json({ "code": "OK", "data": getdeactiveUser });
     } catch (error) {
         res.json({ "code": "ERROR", message: error.message });
